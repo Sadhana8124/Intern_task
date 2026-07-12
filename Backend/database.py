c2eed600a89c410f0375bab99a4133cba7eeac8f
Sadhana8124 <sadhanasubbannavar8124@gmail.com>
2026-07-12 17:33:01 +0530

    Add pymysql and support DATABASE_URL environment variable

--- Backend/database.py
+++ Backend/database.py
@@ -1,8 +1,12 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:admin@localhost:3306/InternTaskApp"
+SQLALCHEMY_DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "mysql+pymysql://root:admin@localhost:3306/InternTaskApp"
+)
 
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
