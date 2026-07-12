db712ec1b5e444bbcac810f06c84da502d03ed79
Sadhana8124 <sadhanasubbannavar8124@gmail.com>
2026-07-12 16:11:00 +0530

    Remove GitHub Pages base path for Vercel deployment

--- my-react-app/vite.config.js
+++ my-react-app/vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: '/Intern_task/',
+  //base: '/Intern_task/',
 })
